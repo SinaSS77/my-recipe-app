499c973dd3c133ed4bc83bc3ef3a1aeec4989d67
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-12-06 19:38:35 -0800

    Created App.tsx and added the fake list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import recipes from './recipes';
+import RecipeList from './components/RecipeList';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="mx-auto w-2/3 p-4 place-content-center ">
+      <input
+        type="text"
+        placeholder="Search recipes..."
+        className="p-2 mb-4 mx-10 w-2/3 border border-gray-300 rounded"
+        value={searchKeyword}
+        onChange={handleSearch}
+      />
+      <RecipeList recipes={recipes} searchKeyword={searchKeyword} />
+    </div>
+  );
+};
 
-export default App
+export default App;
